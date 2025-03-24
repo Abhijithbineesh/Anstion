86855989ee4abdd97bfd73a772289c3f034f1df5
Abhijithbineesh <abhijith.kpbineesh@gmail.com>
2025-03-24 18:15:12 +0530

    Add auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home(){
+
+    return (
+      <div>
+        <h1 className="text-red-600">Anstion</h1>
+        <h1 className="text-blue-700">Anstion</h1>
+        <h1>Anstion</h1>
+      </div>
+    )
+  }
