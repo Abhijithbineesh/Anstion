e9e79cd4ea40837983f0571693e92a04b0fe4f3d
Abhijithbineesh <abhijith.kpbineesh@gmail.com>
2025-03-26 10:27:26 +0530

    implement theme

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home(){
 
     return (
@@ -5,6 +7,7 @@ export default function Home(){
         <h1 className="text-red-600">Anstion</h1>
         <h1 className="text-blue-700">Anstion</h1>
         <h1>Anstion</h1>
+        <UserButton/>
       </div>
     )
   }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,55 +1,47 @@
-import { type Metadata } from 'next'
-import {
-  ClerkProvider
-} from '@clerk/nextjs'
-import './global.css'
-import React from "react"
-import {Inter,Space_Grotesk} from 'next/font/google'
+import { type Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./global.css";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { ThemeProvider } from "@/context/Themeprovider";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
-const inter=Inter({
-  subsets:['latin'],
-  weight:['100','200','300','400','500','600','700','800','900'],
-  variable:'--font-inter'
-})
-
-const spaceGrotesk=Space_Grotesk({
-  subsets:['latin'],
-  weight:['300','400','500','600','700'],
-  variable:'--font-spaceGrotesk'
-})
-
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'Anstion',
-  description: 'Anstion is a dynamic Q&A platform where users can sign in, ask questions on any topic, and receive answers from knowledgeable individuals.',
-
- 
-
-}
+  title: "Anstion",
+  description:
+    "Anstion is a dynamic Q&A platform where users can sign in, ask questions on any topic, and receive answers from knowledgeable individuals.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-
-    appearance={{
-      elements:{
-        formButtonPrimary:'primary-gradient',
-        footerActionLink:'primary-text-gradient hover:text-primary-500'
-
-      }
-    }}
-    
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
